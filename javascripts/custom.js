90c0ac95b465092a3472da42cc44e7b21d47ec10
Ricky Johnson <ricky.johnson@techsoft3d.com>
2022-07-21 15:14:32 -0400

    first draft of functionality

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,4 +1,359 @@
-function loadModel() {
-    viewer.model.clear()
+cameraViews = {
+    HotelFloorplan:
+        [
+            {
+                "position": {
+                    "x": 111.68142382801221,
+                    "y": 532.3969293197923,
+                    "z": 1023.431174023395
+                },
+                "target": {
+                    "x": 111.68142382801221,
+                    "y": 532.3969293197923,
+                    "z": 34.83437252044678
+                },
+                "up": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0
+                },
+                "width": 128.41521599829338,
+                "height": 90.70650297912894,
+                "projection": 0,
+                "nearLimit": 0.01,
+                "className": "Communicator.Camera"
+            },
+            {
+                "position": {
+                    "x": 173.1911879183776,
+                    "y": 231.383955695612,
+                    "z": 1023.431174023395
+                },
+                "target": {
+                    "x": 173.1911879183776,
+                    "y": 231.383955695612,
+                    "z": 34.83437252044678
+                },
+                "up": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0
+                },
+                "width": 215.2019862421738,
+                "height": 152.00861871734594,
+                "projection": 0,
+                "nearLimit": 0.01,
+                "className": "Communicator.Camera"
+            },
+            {
+                "position": {
+                    "x": 693.1768878602618,
+                    "y": 224.4211209483585,
+                    "z": 1023.431174023395
+                },
+                "target": {
+                    "x": 693.1768878602618,
+                    "y": 224.4211209483585,
+                    "z": 34.83437252044678
+                },
+                "up": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0
+                },
+                "width": 184.64860254718118,
+                "height": 130.42713736712068,
+                "projection": 0,
+                "nearLimit": 0.01,
+                "className": "Communicator.Camera"
+            },
+            {
+                "position": {
+                    "x": 522.5765952690625,
+                    "y": 478.006917373565,
+                    "z": 1022.1621568543712
+                },
+                "target": {
+                    "x": 522.5765952690625,
+                    "y": 478.006917373565,
+                    "z": 26.807723999023438
+                },
+                "up": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0
+                },
+                "width": 220.43932800000007,
+                "height": 155.70803200000006,
+                "projection": 0,
+                "nearLimit": 0.01,
+                "className": "Communicator.Camera"
+            },
+            {
+                "position": {
+                    "x": 227.0673986668312,
+                    "y": 436.98898051102003,
+                    "z": 1022.1621568543712
+                },
+                "target": {
+                    "x": 227.0673986668312,
+                    "y": 436.98898051102003,
+                    "z": 26.807723999023438
+                },
+                "up": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0
+                },
+                "width": 235.13528320000006,
+                "height": 166.08856746666675,
+                "projection": 0,
+                "nearLimit": 0.01,
+                "className": "Communicator.Camera"
+            },
+            {
+                "position": {
+                    "x": 530.6343491353796,
+                    "y": 77.83448562668524,
+                    "z": 1022.1621568543712
+                },
+                "target": {
+                    "x": 530.6343491353796,
+                    "y": 77.83448562668524,
+                    "z": 26.807723999023438
+                },
+                "up": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0
+                },
+                "width": 201.75186210203793,
+                "height": 142.50808004751184,
+                "projection": 0,
+                "nearLimit": 0.01,
+                "className": "Communicator.Camera"
+            }
+        ],
+    Clinic_Architectural_First_Floor_With_Perspective: [
+        {
+            "position": {
+                "x": 116.43349980950687,
+                "y": 155.25351917023485,
+                "z": 573.7667196286156
+            },
+            "target": {
+                "x": 116.43349980950687,
+                "y": 155.25351917023485,
+                "z": 218.32234287261963
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 62.27877120000003,
+            "height": 44.03459840000003,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 175.5677541890362,
+                "y": 50.5162094405424,
+                "z": 573.7667196286156
+            },
+            "target": {
+                "x": 175.5677541890362,
+                "y": 50.5162094405424,
+                "z": 218.32234287261963
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 111.93852672595732,
+            "height": 79.14684209224089,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 59.57531230205771,
+                "y": 66.01451544484149,
+                "z": 573.7667196286156
+            },
+            "target": {
+                "x": 59.57531230205771,
+                "y": 66.01451544484149,
+                "z": 218.32234287261963
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 67.9036181093202,
+            "height": 48.01168192205567,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        }
+    ],
+    Chieasa_Dives: [
+        {
+            "position": {
+                "x": 100.53144487100109,
+                "y": 141.00553172342637,
+                "z": 377.89573661322396
+            },
+            "target": {
+                "x": 100.53144487100109,
+                "y": 141.00553172342637,
+                "z": 57.36418437957764
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 103.80902400000005,
+            "height": 73.40032000000002,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 187.30390554732924,
+                "y": 77.11326385211346,
+                "z": 377.89573661322396
+            },
+            "target": {
+                "x": 187.30390554732924,
+                "y": 77.11326385211346,
+                "z": 57.36418437957764
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 89.07069840803521,
+            "height": 62.979281702651136,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 94.64162554052814,
+                "y": 50.610353812825664,
+                "z": 377.89573661322396
+            },
+            "target": {
+                "x": 94.64162554052814,
+                "y": 50.610353812825664,
+                "z": 57.36418437957764
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 78.71517215933962,
+            "height": 55.657192435896675,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 119.87340681114559,
+                "y": 154.6722687306501,
+                "z": 377.30506707530975
+            },
+            "target": {
+                "x": 119.87340681114559,
+                "y": 154.6722687306501,
+                "z": 56.50900173187256
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 97.32096000000004,
+            "height": 68.81280000000002,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 185.06983886231419,
+                "y": 154.2284873251384,
+                "z": 377.30506707530975
+            },
+            "target": {
+                "x": 185.06983886231419,
+                "y": 154.2284873251384,
+                "z": 56.50900173187256
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 83.503779757533,
+            "height": 59.04307659623545,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        },
+        {
+            "position": {
+                "x": 183.69135514230797,
+                "y": 84.68484389345257,
+                "z": 377.30506707530975
+            },
+            "target": {
+                "x": 183.69135514230797,
+                "y": 84.68484389345257,
+                "z": 56.50900173187256
+            },
+            "up": {
+                "x": 0,
+                "y": 1,
+                "z": 0
+            },
+            "width": 55.346605424535646,
+            "height": 39.13396343148986,
+            "projection": 0,
+            "nearLimit": 0.01,
+            "className": "Communicator.Camera"
+        }
+    ],
+}
 
+
+
+function setView(val) {
+    var model_name = Sample._getParameterByName("instance");
+    var availableSheets = viewer.sheetManager.getSheetIds();
+
+    if (availableSheets[0] == viewer.sheetManager._activeSheetId) {
+        var camera = Communicator.Camera.construct(cameraViews[model_name][val]);
+    }
+    else {
+        var camera = Communicator.Camera.construct(cameraViews[model_name][val + 3]);
+    }
+    viewer.getView().setCamera(camera, 500);
+}
+
+function switchSheet(index) {
+    var sheet = viewer.sheetManager.getSheetIds();
+    viewer.sheetManager.setActiveSheetId(sheet[index]);
 }
